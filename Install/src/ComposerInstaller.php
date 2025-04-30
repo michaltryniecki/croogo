1e7a6e7da9371e3e7ffbd971ddbd09853ad076f2
michaltryniecki <michaltryniecki@fabrykawchmurach.pl>
2025-04-30 11:00:50 +0200

    Fix composer installer for croogo

--- Install/src/ComposerInstaller.php
+++ Install/src/ComposerInstaller.php
@@ -23,59 +23,23 @@ class ComposerInstaller extends PluginInstaller
         $config = $composer->getConfig();
         $vendorDir = realpath($config->get('vendor-dir'));
         $croogoDir = dirname(dirname(__DIR__));
-        $packages = $composer->getRepositoryManager()->getLocalRepository()->getPackages();
-        $pluginsDir = dirname($vendorDir) . DIRECTORY_SEPARATOR . 'plugins';
-        $plugins = static::determinePlugins($packages, $pluginsDir, $vendorDir);
+
         $corePlugins = [
             'Acl', 'Core', 'Dashboards', 'Extensions', 'FileManager', 'Install', 'Menus',
             'Settings', 'Users'
         ];
-        foreach ($corePlugins as $plugin) {
-            $plugins['Croogo\\' . $plugin] = $croogoDir . DIRECTORY_SEPARATOR . $plugin;
-        }
-        $configFile = static::_configFile($vendorDir);
-        static::writeConfigFile($configFile, $plugins);
-    }
 
-    /**
-     * Find all plugins available
-     *
-     * Add all composer packages of type cakephp-plugin, and all plugins located
-     * in the plugins directory to a plugin-name indexed array of paths
-     *
-     * @param array $packages an array of \Composer\Package\PackageInterface objects
-     * @param string $pluginsDir the path to the plugins dir
-     * @param string $vendorDir the path to the vendor dir
-     * @return array plugin-name indexed paths to plugins
-     */
-    public static function determinePlugins($packages, $pluginsDir = 'plugins', $vendorDir = 'vendor')
-    {
         $plugins = [];
-
-        foreach ($packages as $package) {
-            if (!in_array($package->getType(), ['cakephp-plugin', 'croogo-plugin', 'croogo-theme'])) {
-                continue;
-            }
-
-            $ns = static::primaryNamespace($package);
-            $path = $vendorDir . DIRECTORY_SEPARATOR . $package->getPrettyName();
-            $plugins[$ns] = $path;
+        foreach ($corePlugins as $plugin) {
+            $plugins[] = "'Croogo/" . $plugin . "' => \$baseDir . '" . $croogoDir . DIRECTORY_SEPARATOR . $plugin . DIRECTORY_SEPARATOR . ",";
         }
+        $plugins[] = "],";
 
-        if (is_dir($pluginsDir)) {
-            $dir = new DirectoryIterator($pluginsDir);
-            foreach ($dir as $info) {
-                if (!$info->isDir() || $info->isDot()) {
-                    continue;
-                }
+        $cakephp_plugins = file_get_contents( $vendorDir . DIRECTORY_SEPARATOR . 'cakephp-plugins.php' );
 
-                $name = $info->getFilename();
-                $plugins[$name] = $pluginsDir . DIRECTORY_SEPARATOR . $name;
-            }
+        if ($cakephp_plugins !== false) {
+            $cakephp_plugins = str_replace("],", implode("\n", $plugins), $cakephp_plugins);
+            file_put_contents($vendorDir . DIRECTORY_SEPARATOR . 'cakephp-plugins.php', $cakephp_plugins);
         }
-
-        ksort($plugins);
-
-        return $plugins;
     }
 }
